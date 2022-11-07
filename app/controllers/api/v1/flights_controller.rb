89813cc715bfa8292f3da53ab8b7bd29c9a2e363
abiodunraheem <raheemabiodun23@gmail.com>
2022-11-08 00:24:23 +0100

    Add methods to list and show flights

--- app/controllers/api/v1/flights_controller.rb
+++ app/controllers/api/v1/flights_controller.rb
@@ -9,24 +9,7 @@ class Api::V1::FlightsController < ApplicationController
     render json: { flight: @flight }, status: :ok
   end
 
-  def create
-    @flight = Flight.new(flight_params)
-    if @flight.save
-      render json: { status: 201, message: 'flight created successfully!', content: { flight: @flight } }
-    else
-      render json: { error: 401, message: ' flight cannot be processed !' }
-    end
-  end
-
-  def destroy
-    @flight = Flight.find(params[:id])
-    if @flight.destroy!
-      render json: { success: 'The flight has been deleted successfully' }, status: :ok
-    else
-      render json: { error: 'There was an error, please try again!' }, status: :internal_server_error
-    end
-  end
-
+  
   private
 
   def flight_params
