a349f2b41f215f896539db328c72ce59c65583c3
abiodunraheem <raheemabiodun23@gmail.com>
2022-11-07 14:00:52 +0100

    Refactor user controller code

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,17 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[ show update destroy ]
 
   # GET /users
   def index
     @users = User.all
-
     render json: @users
   end
 
   # GET /users/1
   def show
-    render json: @user
+    @user = User.find(params[:id])
+    render json: { user: @user }, status: :ok
+    # render json: @user
   end
 
   # POST /users
@@ -18,9 +19,11 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: { status: 201, message: 'user created successfully!', content: { user: @user } }
+      # render json: @user, status: :created, location: @user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { error: 401, message: ' user cannot be processed !' }
+      # render json: @user.errors, status: :unprocessable_entity
     end
   end
 
@@ -35,7 +38,12 @@ class UsersController < ApplicationController
 
   # DELETE /users/1
   def destroy
-    @user.destroy
+    @user = User.find(params[:id])
+    if @user.destroy!
+      render json: { success: 'The user has been deleted successfully' }, status: :ok
+    else
+      render json: { error: 'There was an error, please try again!' }, status: :internal_server_error
+    end
   end
 
   private
