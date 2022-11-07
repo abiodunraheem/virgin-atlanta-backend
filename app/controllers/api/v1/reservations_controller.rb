69cf3aa7a3d6504cde56e824dabd8e3ac6def3e3
abiodunraheem <raheemabiodun23@gmail.com>
2022-11-08 00:25:46 +0100

    Add methods to list reservations

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -10,18 +10,7 @@ class Api::V1::ReservationsController < ApplicationController
     render json: @reservation = Reservation.all
   end
 
-  def create
-    @user = User.find(params[:user_id])
-    @reservation = Reservation.new(reservation_params)
-    @reservation.user_id = @user.id
-    @reservation.doctor_id = reservation_params[:doctor_id]
-    if @reservation.save
-      render json: @reservation
-    else
-      render json: { error: 'Error creating reservation' }
-    end
-  end
-
+  
   private
 
   def reservation_params
