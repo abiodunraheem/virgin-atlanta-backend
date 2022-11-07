613a051468c1b858461380665fee4e222a215e39
abiodunraheem <raheemabiodun23@gmail.com>
2022-11-07 14:02:19 +0100

    Refactor reservation controller code

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,16 +1,16 @@
-class ReservationsController < ApplicationController
+class Api::V1::ReservationsController < ApplicationController
   before_action :set_reservation, only: %i[ show update destroy ]
 
   # GET /reservations
   def index
     @reservations = Reservation.all
-
     render json: @reservations
   end
 
   # GET /reservations/1
   def show
-    render json: @reservation
+    @reservation = Reservation.find(params[:id])
+    render json: { reservation: @reservation }, status: :ok
   end
 
   # POST /reservations
@@ -18,9 +18,11 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new(reservation_params)
 
     if @reservation.save
-      render json: @reservation, status: :created, location: @reservation
+      render json: { status: 201, message: 'reservation created successfully!', content: { reservation: @reservation } }
+      # render json: @reservation, status: :created, location: @reservation
     else
-      render json: @reservation.errors, status: :unprocessable_entity
+      render json: { error: 401, message: ' reservation cannot be processed !' }
+      # render json: @reservation.errors, status: :unprocessable_entity
     end
   end
 
@@ -35,7 +37,12 @@ class ReservationsController < ApplicationController
 
   # DELETE /reservations/1
   def destroy
-    @reservation.destroy
+    @reservation = Reservation.find(params[:id])
+    if @reservation.destroy!
+      render json: { success: 'The reservation has been deleted successfully' }, status: :ok
+    else
+      render json: { error: 'There was an error, please try again!' }, status: :internal_server_error
+    end
   end
 
   private
@@ -46,6 +53,6 @@ class ReservationsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def reservation_params
-      params.require(:reservation).permit(:user_id, :flight_id, :city, :date)
+      params.require(:reservation).permit(:user_id, :flight_id, :date, :city)
     end
 end
