bc2a5560251d0e8378bb88a334efe4de21962334
abiodunraheem <raheemabiodun23@gmail.com>
2022-11-08 00:30:27 +0100

    Remove commented code from routes file

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,6 @@ Rails.application.routes.draw do
       resources :reservations
     end
   end
-  
-  
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
 
 # Rails.application.routes.draw do
