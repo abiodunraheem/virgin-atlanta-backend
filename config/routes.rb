dd716591d4ff85ca831d3a7dfdef1d8416d254c6
abiodunraheem <raheemabiodun23@gmail.com>
2022-11-16 01:27:12 +0100

    Remove commented code from route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,3 @@
-# Rails.application.routes.draw do
-#   # mount Rswag::Ui::Engine => '/api-docs'
-#   # mount Rswag::Api::Engine => '/api-docs'
-#   post 'api/v1/create/:username/:email', to: 'users#create'
-#   get 'api/v1/login/:username', to: 'users#login'
-#   # get 'api/v1/users/flight/:id', to: 'users#user_flight'
-#   # get 'splash', to: 'users#flight'
-#   get 'users/:id', to: 'users#index'
-#   namespace :api do
-#     namespace :v1 do
-#       get 'reservations/:user_id', to: 'reservations#index'
-#       post 'reservations/:user_id', to: 'reservations#create'
-#       get 'api/v1/flight/:id', to: 'flights#show'
-#       delete 'api/v1/flights/:id', to: 'flights#delete'
-#       resources :flights, only: %i[index show create destroy] 
-#       resources :users do
-#         resources :reservations, only: %i[index show]
-#       end
-#     end
-#   end
-# end
-
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
