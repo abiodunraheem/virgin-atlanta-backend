e3aaff95c6324a0412c61ba27b4b0b12399ec683
abiodunraheem <raheemabiodun23@gmail.com>
2022-11-16 01:01:13 +0100

    Update rout.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -23,10 +23,13 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  root "users#index"
-  resources :users, only: [:create, :index]
-  resources :reservations, only: [:create, :index]
-  resources :flights, only: [:index, :show, :create, :destroy]
-
+  root "api/v1/users#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create, :index]
+      resources :reservations, only: [:create, :index]
+      resources :flights, only: [:index, :show, :create, :destroy]
+    end
+  end
   post "/login", to: "users#login"
 end
---
+++ spec/requests/api/v1/flights_spec.rb
@@ -0,0 +1,4 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/flights', type: :request do
+end
